22334cb5b2d91de51b4ab25520ed1de02e3f23f9
shannone <shannone426@gmail.com>
2023-01-02 16:51:54 -0500

    palindrome all tests passing .every isnt ideal solution but whatevs

--- palindrome/index.js
+++ palindrome/index.js
@@ -16,11 +16,13 @@
 // palindrome("abba");
 // palindrome("abcdefg");
 
-
+//NOT and ideal solution
 //.every returns a boolean
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
 function palindrome(str) {
-   
+   return str.split("").every((char, i ) => {
+    return char === str[str.length -1 -i];
+   });
   };
 
 module.exports = palindrome;
