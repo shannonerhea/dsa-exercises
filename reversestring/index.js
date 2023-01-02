50a3219bf6925f3f296a08b5430b238c07a415c9
shannone <shannone426@gmail.com>
2023-01-02 12:22:13 -0500

    i love debugging

--- reversestring/index.js
+++ reversestring/index.js
@@ -15,22 +15,24 @@
 // };
 
 // used for of loop
-// function reverse(str) {
-// let reversed = "";
-// for (let character of str ) {
-//     reversed = character + reversed;
-// };
-// return reversed;
-// };
+function reverse(str) {
+let reversed = "";
+for (let Character of str ) {
+    reversed = Character + reversed;
+    debugger;
+};
+return reversed;
+};
+reverse("asdf");
 
 // when ever reduce run its take the starting arg which is an empty str
 // its passes the empty str into the arrow function and whatever gets returned from the inner function will be
 // will be used for the starting arg for every run of the function
 // function runs one tim for every ele in arr
-function reverse(str) {
-    debugger;
-  return str.split("").reduce((rev, char) => char + rev, "");
-};
-reverse("asdf");
+// function reverse(str) {
+//     debugger;
+//   return str.split("").reduce((rev, char) => char + rev, "");
+// };
+// reverse("asdf");
 
 module.exports = reverse;
