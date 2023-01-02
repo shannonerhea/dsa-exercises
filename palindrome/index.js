7d70a369116fdb39aec6bf9b4bb5a8beb27f3381
shannone <shannone426@gmail.com>
2023-01-02 12:58:26 -0500

    palindrome agin lol

--- palindrome/index.js
+++ palindrome/index.js
@@ -7,6 +7,12 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    const reversed = str.split("").reverse().join();
+    debugger;
+};
+
+palindrome("abba");
+palindrome("abcdefg");  
 
 module.exports = palindrome;
