90a22b3293004bae2993c31471e71bfc29766a6c
shannone <shannone426@gmail.com>
2023-01-03 08:58:57 -0500

    reversint

--- reverseint/index.js
+++ reverseint/index.js
@@ -7,6 +7,12 @@
 //   reverseInt(500) === 5
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
+//console.log("node index.js runs my code!");
+// .toString() === "
+const myNum = 200
+let res = myNum.toString().split(''); // make int a an arr of str ["2", "0", "0"]
+console.log(res);
+
 
 function reverseInt(n) {}
 
