9dbda9148bbefaa977675adbfcb59b42a599286b
shannone <shannone426@gmail.com>
2023-01-03 11:07:17 -0500

    chunk all tests passing used better solution with for loop and slice. arr methods make this code shorter

--- chunk/index.js
+++ chunk/index.js
@@ -8,19 +8,29 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
+// _function chunk(array, size) {
+//     // size is num of ele in ea arr
+//     let chunked = [];
+//     for (let ele of array) {
+//         const last = chunked[chunked.length -1]
+//    if (!last || last.length === size) {
+//     chunked.push([ele]);
+//    } else {
+//     last.push(ele);
+//    }
+//     }
+//     return chunked;
+// }
+
+
+//arr method .slice https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
 function chunk(array, size) {
-    // size is num of ele in ea arr
-    let chunked = [];
-    for (let ele of array) {
-        const last = chunked[chunked.length -1]
-   if (!last || last.length === size) {
-    chunked.push([ele]);
-   } else {
-    last.push(ele);
-   }
+    const chunked = [];
+    for (let i = 0; i < array.length; i += size) {
+        chunked.push(array.slice(i, i + size));
     }
     return chunked;
-}
 
+}
 
 module.exports = chunk;
