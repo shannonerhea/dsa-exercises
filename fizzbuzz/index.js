ff9da0ef7de5e9b3b1eb527bfc697a839cc5eafc
shannone <shannone426@gmail.com>
2023-01-03 10:42:57 -0500

    fizzbuzz all tests passing used classic fizzbuzz solution. no fancy one liners

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -12,6 +12,22 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+    // question starts at 1 not 0
+    for (let i = 1; i <= n; i++) {
+        //is n multple of 3 && 5
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("fizzbuzz");
+        } else if (i % 3 === 0 ) {
+            // is n multp of 3
+            console.log("fizz");
+        } else if (i % 5 === 0) {
+            // is n multp of 5
+            console.log("buzz");
+        } else {
+            console.log(i);
+        }
+    };
+}
 
 module.exports = fizzBuzz;
