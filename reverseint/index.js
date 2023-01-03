da4631a73d55dc11b7c2fa28d2042d3c46d8048a
shannone <shannone426@gmail.com>
2023-01-03 09:11:00 -0500

    reverseInt MDN .toString, Math.sigh(), ParseInt. also added links to docs

--- reverseint/index.js
+++ reverseint/index.js
@@ -8,12 +8,17 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 //console.log("node index.js runs my code!");
-// .toString() === "
+// .toString()
 const myNum = 200
-let res = myNum.toString().split(''); // make int a an arr of str ["2", "0", "0"]
+//let res = myNum.toString().split(''); // make int a an arr of str ["2", "0", "0"]
+let res = parseInt(myNum.toString());
+//parseInt turns the str back into a integer
 console.log(res);
 
 
+
+// Math.sign(); https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/sign
+// eg. Math.sign(500)= 1 Math.sign(-500)= -1
 function reverseInt(n) {}
 
 module.exports = reverseInt;
